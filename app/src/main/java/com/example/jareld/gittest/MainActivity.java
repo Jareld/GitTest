3503c0e92f295413797c11d40b1ea67359a60800
jareld <763165028@qq.com>
2017-04-13 20:21:54 +0800

    add a log

--- app/src/main/java/com/example/jareld/gittest/MainActivity.java
+++ app/src/main/java/com/example/jareld/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jareld.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity
         extends AppCompatActivity
@@ -11,5 +12,6 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Jareld", "onCreate:  ");
     }
 }
